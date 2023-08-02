6178cf06ae05c0bc021de0523e012173e07181f7
Juan diego ramirez <juandiegoramirez098@gmail.com>
2023-08-02 12:59:56 -0500

    feat: :sparkles: transacciones y crud basico

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
+import "dotenv/config";
+
 const app = express();
-const connection = new MongoClient('mongodb+srv://myAtlasDBUser:123@myatlasclusteredu.mhc53io.mongodb.net/?retryWrites=true&w=majority')
+const connection = new MongoClient(process.env.URI_MONGODB)
 const dbName = "campus";
+const db = connection.db(dbName).collection("M3");
 
 const main = async() => {
-
+    
     await connection.connect();
-    const db = connection.db(dbName);
 
+    //! Cruda basico
+    //const insertUser = { name: "Leslie", age: 18, ratings: [34, 43, 54] };
+    //let newUser = await db.insertOne(insertUser);
+    //let countUsers = await db.countDocuments();
+    //const updateUser = await db.updateOne({ name: "Daniela" }, { $pull: { ratings: 13 } })
+    //const deleteUser = await db.deleteOne({ _id: new ObjectId("64ca8acab505b1ffe13500bc") })
+    //let users = await db.find().toArray();
+
+    //! transacciones
+
+    const session = connection.startSession()
+
+    await session.withTransaction(async() => {
+
+        const updateUser = await db.updateOne(
+            { name: "Daniela" },
+            { $pull: { ratings: 13 }},
+            { session }
+            )
+        const deleteUser = await db.deleteOne(
+            { _id: new ObjectId("64ca8acab505b1ffe13500bc") },
+            { session }
+            )
+    })
+
+    await session.endSession()
     return true;
 }
 
@@ -17,6 +45,8 @@ main()
     .catch((err) => console.error(`Error connecting to database: ${err}`))
     .finally(() => connection.close());
     
+
+
 //app.get()
 
 
